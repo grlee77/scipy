4407b6e463252f750949ecebaf51a72104c13398
Gregory R. Lee <gregory.lee@cchmc.org>
2016-11-22 20:44:14 -0500

    fixes

--- scipy/signal/_upfirdn_apply.pyx
+++ scipy/signal/_upfirdn_apply.pyx
@@ -36,6 +36,7 @@
 cimport cython
 cimport numpy as np
 import numpy as np
+cimport numpy as cnp
 from cython import bint  # boolean integer type
 from libc.stdlib cimport malloc, free
 
@@ -75,6 +76,22 @@ def _output_len(Py_ssize_t len_h,
 cpdef _apply_axis(np.ndarray data, np.ndarray h_trans_flip, np.ndarray out,
                   Py_ssize_t up, Py_ssize_t down, Py_ssize_t axis):
     cdef ArrayInfo data_info, output_info
+    cdef Py_ssize_t len_h = h_trans_flip.size
+    # TODO: is their a way to keep nogil below and not have to declare all
+    #       possible pointer types up here?
+    # (cannot use cnp.PyArray_DATA within nogil block)
+    cdef double *d_dptr
+    cdef double *d_hptr
+    cdef double *d_optr
+    cdef float *s_dptr
+    cdef float *s_hptr
+    cdef float *s_optr
+    cdef float_complex *c_dptr
+    cdef float_complex *c_hptr
+    cdef float_complex *c_optr
+    cdef double_complex *z_dptr
+    cdef double_complex *z_hptr
+    cdef double_complex *z_optr
 
     data_info.ndim = data.ndim
     data_info.strides = <Py_ssize_t *> data.strides
@@ -85,50 +102,60 @@ cpdef _apply_axis(np.ndarray data, np.ndarray h_trans_flip, np.ndarray out,
     output_info.shape = <Py_ssize_t *> out.shape
 
     if data.dtype == np.float64:
+        d_dptr = <double*> cnp.PyArray_DATA(data)
+        d_hptr = <double*> cnp.PyArray_DATA(h_trans_flip)
+        d_optr = <double*> cnp.PyArray_DATA(out)
         with nogil:
-            _apply_axis_inner(<double *> data.data, input_info,
-                              <double *> h_trans_flip.data,
-                              <double *> output.data, output_info,
-                              up, down, axis)
+            retval = _apply_axis_inner(d_dptr, data_info,
+                                       d_hptr, len_h,
+                                       d_optr, output_info,
+                                       up, down, axis)
         if retval:
             raise RuntimeError("_apply_axis_inner failed")
     elif data.dtype == np.float32:
+        s_dptr = <float*> cnp.PyArray_DATA(data)
+        s_hptr = <float*> cnp.PyArray_DATA(h_trans_flip)
+        s_optr = <float*> cnp.PyArray_DATA(out)
         with nogil:
-            _apply_axis_inner(<float *> data.data, input_info,
-                              <float *> h_trans_flip.data,
-                              <float *> output.data, output_info,
-                              up, down, axis)
-        if retval:
-            raise RuntimeError("_apply_axis_inner failed")
+            retval = _apply_axis_inner(s_dptr, data_info,
+                                       s_hptr, len_h,
+                                       s_optr, output_info,
+                                       up, down, axis)
     elif data.dtype == np.complex128:
+        z_dptr = <double_complex*> cnp.PyArray_DATA(data)
+        z_hptr = <double_complex*> cnp.PyArray_DATA(h_trans_flip)
+        z_optr = <double_complex*> cnp.PyArray_DATA(out)
         with nogil:
-            _apply_axis_inner(<double_complex *> data.data, input_info,
-                              <double_complex *> h_trans_flip.data,
-                              <double_complex *> output.data, output_info,
-                              up, down, axis)
+            retval = _apply_axis_inner(z_dptr, data_info,
+                                       z_hptr, len_h,
+                                       z_optr, output_info,
+                                       up, down, axis)
         if retval:
             raise RuntimeError("_apply_axis_inner failed")
     elif data.dtype == np.complex64:
+        c_dptr = <float_complex*> cnp.PyArray_DATA(data)
+        c_hptr = <float_complex*> cnp.PyArray_DATA(h_trans_flip)
+        c_optr = <float_complex*> cnp.PyArray_DATA(out)
         with nogil:
-            _apply_axis_inner(<float_complex *> data.data, input_info,
-                              <float_complex *> h_trans_flip.data,
-                              <float_complex *> output.data, output_info,
-                              up, down, axis)
+            retval = _apply_axis_inner(c_dptr, data_info,
+                                       c_hptr, len_h,
+                                       c_optr, output_info,
+                                       up, down, axis)
         if retval:
             raise RuntimeError("_apply_axis_inner failed")
-    return output
+    return out
 
 
 cdef int _apply_axis_inner(DTYPE_t* input, ArrayInfo input_info,
-                           DTYPE_t* h_trans_flip,
+                           DTYPE_t* h_trans_flip, Py_ssize_t len_h,
                            DTYPE_t* output, ArrayInfo output_info,
                            Py_ssize_t up, Py_ssize_t down,
-                           Py_ssize_t axis):
+                           Py_ssize_t axis) nogil:
     cdef Py_ssize_t i
-    cdef num_loops = 1
+    cdef Py_ssize_t num_loops = 1
     cdef bint make_temp_input, make_temp_output  # booleans
-    DTYPE_t* temp_input = NULL
-    DTYPE_t* temp_output = NULL
+    cdef DTYPE_t* temp_input = NULL
+    cdef DTYPE_t* temp_output = NULL
 
     if (input_info.ndim != output_info.ndim):
         return 1
@@ -137,13 +164,15 @@ cdef int _apply_axis_inner(DTYPE_t* input, ArrayInfo input_info,
 
     make_temp_input = input_info.strides[axis] != sizeof(DTYPE_t);
     make_temp_output = output_info.strides[axis] != sizeof(DTYPE_t);
-    if (make_temp_input):
-        if ((temp_input = malloc(input_info.shape[axis] * sizeof(DTYPE_t))) == NULL):
+    if make_temp_input:
+        temp_input = <DTYPE_t*>malloc(input_info.shape[axis] * sizeof(DTYPE_t))
+        if not temp_input:
             free(temp_input)
             free(temp_output)
             return 2
-    if (make_temp_output):
-        if ((temp_output = malloc(output_info.shape[axis] * sizeof(DTYPE_t))) == NULL):
+    if make_temp_output:
+        temp_output = <DTYPE_t*>malloc(output_info.shape[axis] * sizeof(DTYPE_t))
+        if not temp_output:
             free(temp_input)
             free(temp_output)
             return 2
@@ -152,19 +181,22 @@ cdef int _apply_axis_inner(DTYPE_t* input, ArrayInfo input_info,
         if i != axis:
             num_loops *= output_info.shape[i]
 
+    cdef Py_ssize_t j
+    cdef Py_ssize_t input_offset = 0
+    cdef Py_ssize_t output_offset = 0
+    cdef const DTYPE_t* input_row
+    cdef DTYPE_t* output_row
+    cdef Py_ssize_t reduced_idx
+    cdef Py_ssize_t j_rev
+    cdef Py_ssize_t axis_idx
+    cdef DTYPE_t* tmp_ptr = NULL
     for i in range(num_loops):
-        cdef size_t j
-        cdef Py_ssize_t input_offset = 0
-        cdef Py_ssize_t output_offset = 0
-        cdef const DTYPE_t* input_row
-        cdef DTYPE_t* output_row
 
         # Calculate offset into linear buffer
-        cdef Py_ssize_t reduced_idx = i
+        reduced_idx = i
         for j in range(output_info.ndim):
-            cdef Py_ssize_t j_rev = output_info.ndim - 1 - j
+            j_rev = output_info.ndim - 1 - j
             if j_rev != axis:
-                cdef Py_ssize_t axis_idx
                 axis_idx = reduced_idx % output_info.shape[j_rev]
                 reduced_idx /= output_info.shape[j_rev]
                 input_offset += (axis_idx * input_info.strides[j_rev])
@@ -174,8 +206,8 @@ cdef int _apply_axis_inner(DTYPE_t* input, ArrayInfo input_info,
         if make_temp_input:
             for j in range(input_info.shape[axis]):
                 # Offsets are byte offsets, to need to cast to char and back
-                temp_input[j] = *(<DTYPE_t *>((<char *> input) + input_offset +
-                    j * input_info.strides[axis]))
+                temp_input[j] = (<DTYPE_t *>((<char *> input) + input_offset +
+                    j * input_info.strides[axis]))[0]
 
         # Select temporary or direct output and input
         if make_temp_input:
@@ -188,14 +220,17 @@ cdef int _apply_axis_inner(DTYPE_t* input, ArrayInfo input_info,
             output_row = <DTYPE_t *>(<char *> output + output_offset)
 
         # call 1D upfirdn
-        _apply_impl(input_row, h_trans_flip, output_row, up, down)
+        _apply_impl(input_row, input_info.shape[axis],
+                    h_trans_flip, len_h, output_row, up, down)
 
         # Copy from temporary output if necessary
         if make_temp_output:
             for j in range(output_info.shape[axis]):
                 # Offsets are byte offsets, to need to cast to char and back
-                *(<DTYPE_t *>((<char *> output) + output_offset +
-                    j * output_info.strides[axis])) = output_row[j]
+                tmp_ptr = (<DTYPE_t *>((<char *> output) + output_offset +
+                    j * output_info.strides[axis]))
+                tmp_ptr[0] = output_row[j]
+
     # cleanup
     free(temp_input)
     free(temp_output)
@@ -205,16 +240,18 @@ cdef int _apply_axis_inner(DTYPE_t* input, ArrayInfo input_info,
 def _apply(DTYPE_t [:] x, DTYPE_t [:] h_trans_flip, DTYPE_t [:] out,
                  Py_ssize_t up, Py_ssize_t down):
     """legacy version without axis support"""
-    _apply_impl(&x[0], &h_trans_flip[0], &out[0], up, down)
+    _apply_impl(&x[0], len(x), &h_trans_flip[0], len(h_trans_flip),
+                &out[0], up, down)
 
 
 @cython.cdivision(True)  # faster modulo
 @cython.boundscheck(False)  # designed to stay within bounds
 @cython.wraparound(False)  # we don't use negative indexing
-cdef void _apply_impl(DTYPE_t *x, DTYPE_t *h_trans_flip, DTYPE_t *out,
+cdef void _apply_impl(DTYPE_t *x, Py_ssize_t len_x, DTYPE_t *h_trans_flip,
+                      Py_ssize_t len_h, DTYPE_t *out,
                       Py_ssize_t up, Py_ssize_t down) nogil:
-    cdef Py_ssize_t len_x = x.shape[0]
-    cdef Py_ssize_t h_per_phase = h_trans_flip.shape[0] / up
+    #cdef Py_ssize_t len_x = x.shape[0]
+    cdef Py_ssize_t h_per_phase = len_h / up
     cdef Py_ssize_t padded_len = len_x + h_per_phase - 1
     cdef Py_ssize_t x_idx = 0
     cdef Py_ssize_t y_idx = 0
